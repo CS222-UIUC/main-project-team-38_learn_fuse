54e8642a928eb9ce30690cd3dd5c3faa71828e0c
AJ <ajindal3@illinois.edu>
2024-10-06 20:32:59 -0500

    set-up backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000; // Use the port from the environment variable or default to 5000
+const PORT = process.env.PORT || 3000; // Use the port from the environment variable or default to 5000
 
 // Define a simple route
 app.get('/', (req, res) => {
