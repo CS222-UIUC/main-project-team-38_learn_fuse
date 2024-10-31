10a9171c035720e3c11f96aaa5dca08f953a9e56
Prisha Thoguluva <prishat2@illinois.edu>
2024-10-31 16:31:58 -0500

    Fix Prettier and ESLint issues

--- public/visual.js
+++ public/visual.js
@@ -1,25 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const uploadButton = document.getElementById("uploadButton");
-    const submitButton = document.getElementById("submitButton");
-    const fileInput = document.getElementById("fileInput");
-    const outputText = document.getElementById("output-text");
-  
-    uploadButton.addEventListener("click", function () {
-      fileInput.click(); // Simulate click on the file input
-    });
-  
-    fileInput.addEventListener("change", function () {
-      if (fileInput.files.length > 0) {
-        submitButton.disabled = false;     }
-    });
-  
-  
-    submitButton.addEventListener("click", function () {
-      outputText.textContent = 'Diagram loading...';
-      
-      setTimeout(() => {
-        outputText.textContent = 'Diagram generated successfully!';     }, 2000);   });
+document.addEventListener('DOMContentLoaded', function () {
+  const uploadButton = document.getElementById('uploadButton');
+  const submitButton = document.getElementById('submitButton');
+  const fileInput = document.getElementById('fileInput');
+  const outputText = document.getElementById('output-text');
+
+  uploadButton.addEventListener('click', function () {
+    fileInput.click(); // Simulate click on the file input
   });
-  
-  
-  
+
+  fileInput.addEventListener('change', function () {
+    if (fileInput.files.length > 0) {
+      submitButton.disabled = false;
+    }
+  });
+
+  submitButton.addEventListener('click', function () {
+    outputText.textContent = 'Diagram loading...';
+
+    setTimeout(() => {
+      outputText.textContent = 'Diagram generated successfully!';
+    }, 2000);
+  });
+});
