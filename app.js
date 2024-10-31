fe7cffcf107f167d758ae7732ae7496bb29fb23c
Prisha Thoguluva <prishat2@illinois.edu>
2024-10-31 09:58:18 -0500

    Add functionality for upload and submit buttons on visual page and dummy output

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/views'));
+
+app.use('/public', express.static(__dirname + '/public'));
+
+const contactRoutes = require('./routes/contact');
+app.use('/api/contact', contactRoutes);
+
+const kinestheticRoutes = require('./routes/kinesthetic');
+app.use('/api/kinesthetic', kinestheticRoutes);
+
 app.get('/', function (request, response) {
-  response.send('testing');
+  // response.send("testing");
+  response.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(3000, function () {
